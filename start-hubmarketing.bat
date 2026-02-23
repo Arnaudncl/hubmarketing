cd7f286a806a1ae5eed5fff782a45713e40e47c8
HubMarketing User <local@hubmarketing.local>
2026-02-24 10:55:46 +1100

    Bind Vite dev server to IPv4 and open localhost URL

--- start-hubmarketing.bat
+++ start-hubmarketing.bat
@@ -15,11 +15,11 @@ timeout /t 2 /nobreak >nul
 
 echo.
 echo [HubMarketing] URLs:
-echo - UI: http://127.0.0.1:5174
+echo - UI: http://localhost:5174
 echo - API: http://127.0.0.1:4000
 echo - Supervisor: http://127.0.0.1:4010/api/supervisor/status
 echo.
 echo [HubMarketing] Ouverture du navigateur...
-start "" "http://127.0.0.1:5174"
+start "" "http://localhost:5174"
 
 endlocal
